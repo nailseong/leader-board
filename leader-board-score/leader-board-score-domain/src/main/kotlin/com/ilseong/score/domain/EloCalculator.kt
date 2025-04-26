b8a2aaeb2887bd97a6974eb7d75dbfe5c6aabf79
나일성 <ilseongdev@gmail.com>
2025-04-26 19:00:18 +0900

    change k factor calculate method

--- leader-board-score/leader-board-score-domain/src/main/kotlin/com/ilseong/score/domain/EloCalculator.kt
+++ leader-board-score/leader-board-score-domain/src/main/kotlin/com/ilseong/score/domain/EloCalculator.kt
@@ -3,6 +3,7 @@ package com.ilseong.score.domain
 import com.ilseong.score.domain.dto.EloCalculateRequest
 import com.ilseong.score.domain.dto.EloCalculateResponse
 import org.springframework.stereotype.Component
+import kotlin.math.exp
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
@@ -11,7 +12,10 @@ import kotlin.math.roundToInt
 class EloCalculator {
 
     companion object {
-        private const val K_FACTOR = 32.0
+        private const val SIGMOID_SLOPE = 12.0
+        private const val MIN_K_FACTOR = 10.0
+        private const val MAX_K_FACTOR = 40.0
+        private const val MEDIAN_EXPECTED_SCORE = 0.5
         private const val MIN_RATING = 100.0
     }
 
@@ -54,7 +58,9 @@ class EloCalculator {
         actualScore: Double,
     ): Int {
         val expectedScore = calculateExpectedScore(playerRating, opponentPlayerRating)
-        val newRating = playerRating + (K_FACTOR * (actualScore - expectedScore))
+//        val kFactor = calculateDynamicKFactor(expectedScore)
+        val kFactor = calculateSigmoidKFactor(expectedScore)
+        val newRating = playerRating + (kFactor * (actualScore - expectedScore))
         return maxOf(newRating, MIN_RATING).roundToInt()
     }
 
@@ -67,5 +73,31 @@ class EloCalculator {
      */
     private fun calculateExpectedScore(playerRating: Int, opponentPlayerRating: Int): Double {
         return 1.0 / (1.0 + 10.0.pow((opponentPlayerRating - playerRating) / 400.0))
+    }
+
+    /**
+     * 기대 승률에 따라 규칙 기반으로 k factor 계산
+     */
+    private fun calculateDynamicKFactor(expectedScore: Double): Int {
+        return when {
+            expectedScore > 0.9 -> 10
+            expectedScore > 0.7 -> 20
+            expectedScore > 0.3 -> 30
+            else -> 40
+        }
+    }
+
+    /**
+     * 기대 승률에 따라 sigmoid 함수를 활용해 k factor 계산
+     */
+    private fun calculateSigmoidKFactor(expectedScore: Double): Int {
+        // Sigmoid 함수 - 기대 승률 0.5에서 중간값을 갖도록 조정
+        // 1 - expectedScore를 사용하여 기대승률이 낮을수록 높은 K 값 적용
+        val sigmoidValue = 1.0 / (1.0 + exp(SIGMOID_SLOPE * (expectedScore - MEDIAN_EXPECTED_SCORE)))
+
+        // minK와 maxK 사이 값으로 매핑
+        val kFactor = MIN_K_FACTOR + (MAX_K_FACTOR - MIN_K_FACTOR) * sigmoidValue
+
+        return kFactor.roundToInt()
     }
 }
